13ec9013a1b35c3eac8a78c2a3013025b2896fec
weathon <wguo6358@gmail.com>
2025-04-03 15:07:25 -0700

    frames

--- run_gasvid.py
+++ run_gasvid.py
@@ -3,3 +3,7 @@ import os
 videos = os.listdir("/home/wg25r/Videos")
 for video in videos:
     os.system(f"python3 owl_gasvid.py --video_id {video} --display ''")
+
+os.system("git add .")
+os.system(f"git commit -m 'update results for {video}'")
+os.system(f"git push")
